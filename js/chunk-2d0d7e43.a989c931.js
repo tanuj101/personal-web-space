3af2df826e0a96e23b12e214482ff691595a0ae4
tanuj <tanujnagpal999@gmail.com>
2019-05-03 18:38:18 +0530

    [REBUILD]

---
+++ js/chunk-2d0d7e43.a989c931.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d7e43"],{"796a":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",[t("h3",[e._v("heading2")])])}],u=t("2877"),a={},l=Object(u["a"])(a,c,r,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0d7e43.a989c931.js.map
