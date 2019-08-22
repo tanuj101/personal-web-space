ab2fd34bc2f07cd66fa09fe2bb9719a9d66ae66d
Tanuj Nagpal <tanujnagpal999@gmail.com>
2019-08-22 15:31:42 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,35 +1,143 @@
-importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw.js')
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
 
-// --------------------------------------------------
-// Configure
-// --------------------------------------------------
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-// Set workbox config
-workbox.setConfig({
-  "debug": false
-})
+workbox.core.skipWaiting();
 
-// Start controlling any existing clients as soon as it activates
-workbox.core.clientsClaim()
+workbox.core.clientsClaim();
 
-// Skip over the SW waiting lifecycle stage
-workbox.core.skipWaiting()
-
-workbox.precaching.cleanupOutdatedCaches()
-
-// Enable offline Google Analytics tracking
-workbox.googleAnalytics.initialize()
-
-// --------------------------------------------------
-// Precaches
-// --------------------------------------------------
-
-// Precache assets
-
-// --------------------------------------------------
-// Runtime Caching
-// --------------------------------------------------
-
-// Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.CacheFirst ({}), 'GET')
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "_nuxt/09ad0d11902e5e080c8e.js",
+    "revision": "ae2c459be3d3492fac52baac49ff1245"
+  },
+  {
+    "url": "_nuxt/16181f98c118da912887.js",
+    "revision": "82fb5f4533b12d62f772fef523f5c592"
+  },
+  {
+    "url": "_nuxt/1af25cdaa7ef91e2b32c.js",
+    "revision": "75617ffd1514656d7ddf480edfefe840"
+  },
+  {
+    "url": "_nuxt/2d6f6603044050b881ab.js",
+    "revision": "a302ed0404f3f86322abbefedfbd26f1"
+  },
+  {
+    "url": "_nuxt/88c251b5372952d3b9dd.js",
+    "revision": "2fa4c80be05aa129debe062b00918f82"
+  },
+  {
+    "url": "_nuxt/ab24cdad04f7cf83f518.js",
+    "revision": "c22baafe16d9e7d5637f6fb86ad990fe"
+  },
+  {
+    "url": "_nuxt/aec5eb5af70a3ce85641.js",
+    "revision": "97944d149e08741c7b38e6459ecfd295"
+  },
+  {
+    "url": "_nuxt/c888a4618e948c4bc83b.js",
+    "revision": "8dd9726521395e9af04a9239f9d1626d"
+  },
+  {
+    "url": "_nuxt/ce9d342a175e4b1f9310.js",
+    "revision": "1ef3c1ea2f77c31953cf39dc94402335"
+  },
+  {
+    "url": "_nuxt/fe77a4eeb98a35d29090.js",
+    "revision": "62061a4787b0b36c0ff2b36cd69a044b"
+  },
+  {
+    "url": "_nuxt/img/279c2b9.jpg",
+    "revision": "279c2b927a89e32298423f2cb497f5bc"
+  },
+  {
+    "url": "_nuxt/img/3d57a04.png",
+    "revision": "3d57a04b932e3e01729894891e5ac27e"
+  },
+  {
+    "url": "_nuxt/img/601b16c.jpg",
+    "revision": "601b16ca94341d749402612ae9a4e571"
+  },
+  {
+    "url": "_nuxt/img/6babd55.jpg",
+    "revision": "6babd55ab85bd9b58ae1e2ff79799b45"
+  },
+  {
+    "url": "_nuxt/img/7df018f.jpeg",
+    "revision": "7df018f47323e6f61c322f19f3f96d21"
+  },
+  {
+    "url": "_nuxt/img/7df3a01.jpeg",
+    "revision": "7df3a01bf8d28356c257aad10287b055"
+  },
+  {
+    "url": "_nuxt/img/9466d56.jpg",
+    "revision": "9466d56c93421b33056f782d7c36c0d8"
+  },
+  {
+    "url": "_nuxt/videos/ce75088.mp4",
+    "revision": "ce75088354b69cbb8dd7845cf596bc4f"
+  },
+  {
+    "url": "200.html",
+    "revision": "504d81d09a1ae706eb7e9356e943b0cb"
+  },
+  {
+    "url": "2019/exploring-cpp/index.html",
+    "revision": "8b879732a3d1f7500658982934a9197b"
+  },
+  {
+    "url": "2019/intro-to-chmod/index.html",
+    "revision": "ea31a859e5e1e6fc57b317d4a962a569"
+  },
+  {
+    "url": "2019/why-arch-linux/index.html",
+    "revision": "65d80dd2cba476674f0fafa6a02abc67"
+  },
+  {
+    "url": "about/index.html",
+    "revision": "2ba60ec5fbd6016b11c24444c8c89322"
+  },
+  {
+    "url": "about/instantpost/index.html",
+    "revision": "9c3426b8b6eae56f1e7be77f91de6587"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "7a01f882c6fda40ad33181cf570d425c"
+  },
+  {
+    "url": "google4a0994fd24c791e9.html",
+    "revision": "559a6db1d513e8cbaad630d34e606363"
+  },
+  {
+    "url": "icon.png",
+    "revision": "7a01f882c6fda40ad33181cf570d425c"
+  },
+  {
+    "url": "index.html",
+    "revision": "28f5dce60936fdf78c25ab9e25dbc722"
+  },
+  {
+    "url": "projects/index.html",
+    "revision": "c7b7eac911192feab2529885537de2be"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
