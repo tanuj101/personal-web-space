6679074e8503f85d47a3952499eddf6964d393de
Tanuj Nagpal <tanujnagpal999@gmail.com>
2020-03-07 22:27:51 +0530

    image viewer

--- components/ImageViewer.vue
+++ components/ImageViewer.vue
@@ -1,16 +1,59 @@
 <template>
   <div>
-    <img :src="src" alt />
+    <div @click="fullWidthImage = !fullWidthImage" :class="{'viewer-container':fullWidthImage}">
+      <img :class="{ full: fullWidthImage,'expand-img':1 }" :src="src" alt />
+    </div>
+    <img @click="fullWidthImage = !fullWidthImage" :src="src" class="paragraph-img" alt />
   </div>
 </template>
 <script>
 export default {
-  props: ["src"]
+  props: ["src"],
+  data() {
+    return {
+      fullWidthImage: false
+    };
+  }
 };
 </script>
 <style scoped>
-img {
-  height: 100px;
-  width: 100px;
+.viewer-container {
+  position: fixed;
+  height: 100%;
+  width: auto;
+  top: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  left: 0;
+  right: 0;
+}
+.expand-img {
+  display: none;
+  height: 100%;
+  width: 100%;
+}
+.viewer-container img {
+  max-height: 60vh;
+  /* height: 100%; */
+  width: auto;
+  display: block !important;
+}
+.full {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  width: 100%;
+  height: auto;
+}
+/* img {
+  width: 150px;
+  border-radius: 2px;
+  box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.5);
+  transition: width 1s;
+} */
+img:hover {
+  cursor: pointer;
 }
 </style>
